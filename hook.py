f7fe4154a3bc72232945d3cbd540115ec311f396
Hunt, David <davidhunt@mitre.org>
2020-01-24 10:40:47 -0500

    adding usb building option

--- hook.py
+++ hook.py
@@ -1,4 +1,5 @@
-from plugins.access.app.access_svc import AccessService
+from plugins.access.app.access_api import AccessApi
+from plugins.access.app.red.red_clone import RedClone
 
 name = 'Access'
 description = 'A toolkit containing a set of functional red-team tools'
@@ -7,11 +8,13 @@ address = '/plugin/access/gui'
 
 async def enable(services):
     props = services.get('app_svc').strip_yml('plugins/access/conf/default.yml')[0]
-    access_svc = AccessService(services=services, props=props)
+    access_api = AccessApi(services=services, props=props)
     app = services.get('app_svc').application
     app.router.add_static('/access', 'plugins/access/static', append_version=True)
-    app.router.add_route('GET', '/plugin/access/gui', access_svc.landing)
-    app.router.add_route('GET', props.get('clone_url'), access_svc.malicious)
-    app.router.add_route('POST', '/plugin/access/props', access_svc.update_props)
-    app.router.add_route('POST', '/plugin/access/log', access_svc.key_log)
-    await access_svc.clone_new_site(url=props.get('clone_site'))
+    app.router.add_route('GET', '/plugin/access/gui', access_api.landing)
+    app.router.add_route('GET', props.get('clone_url'), access_api.malicious)
+    app.router.add_route('POST', '/plugin/access/clone', access_api.clone)
+    app.router.add_route('POST', '/plugin/access/usb', access_api.usb)
+
+    app.router.add_route('POST', '/plugin/access/log', access_api.key_log)
+    await RedClone().action(url=props.get('clone_site'), props=props)
