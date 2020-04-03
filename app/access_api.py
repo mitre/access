a4bb4d93e577e111a81aeb78ca87befd0cf1ebbe
Hunt, David <davidhunt@mitre.org>
2020-04-03 16:42:56 -0400

    update

--- app/access_api.py
+++ app/access_api.py
@@ -10,6 +10,7 @@ class AccessApi:
 
     def __init__(self, services):
         self.data_svc = services.get('data_svc')
+        self.rest_svc = services.get('rest_svc')
         self.auth_svc = services.get('auth_svc')
 
     @check_authorization
@@ -18,10 +19,10 @@ class AccessApi:
         trimmed = defaultdict(list)
         for ex in await self.data_svc.locate('abilities', dict(tactic='initial-access', technique_name=('aux', 'exploit'))):
             trimmed[ex.ability_id] = ex.name
-        agents = await self.data_svc.locate('agents', dict(group='pets'))
-        return dict(exploits=dict(trimmed), agents=[a.display for a in agents])
+        return dict(exploits=dict(trimmed), agents=[a.display for a in await self.data_svc.locate('agents')])
 
     @check_authorization
     async def exploit(self, request):
         data = dict(await request.json())
+        await self.rest_svc.task_agent_with_ability(data['paw'], data['ability_id'])
         return web.json_response('complete')
