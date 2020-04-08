54723f6636997a2f8b5cde7942423e74056afb08
Hunt, David <davidhunt@mitre.org>
2020-04-08 19:26:04 -0400

    ensuring the list of abilities covers more than just pre-attack

--- app/access_api.py
+++ app/access_api.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+import itertools
 
 from aiohttp import web
 from aiohttp_jinja2 import template
@@ -13,15 +13,11 @@ class AccessApi:
         self.data_svc = services.get('data_svc')
         self.rest_svc = services.get('rest_svc')
         self.auth_svc = services.get('auth_svc')
-        self.exploit_tactics = ('initial-access', 'technical-information-gathering')
 
     @check_authorization
     @template('access.html')
     async def landing(self, request):
-        exploits = defaultdict(list)
-        for i in await self.data_svc.locate('abilities', dict(tactic=self.exploit_tactics)):
-            exploits[i.ability_id] = i.name
-        return dict(exploits=dict(exploits), agents=[a.display for a in await self.data_svc.locate('agents')])
+        return dict(agents=[a.display for a in await self.data_svc.locate('agents')])
 
     @check_authorization
     async def exploit(self, request):
@@ -29,3 +25,11 @@ class AccessApi:
         converted_facts = [Fact(trait=f['trait'], value=f['value']) for f in data.get('facts')]
         await self.rest_svc.task_agent_with_ability(data['paw'], data['ability_id'], converted_facts)
         return web.json_response('complete')
+
+    @check_authorization
+    async def abilities(self, request):
+        data = dict(await request.json())
+        agent = (await self.data_svc.locate('agents', dict(paw=data['paw'])))[0]
+        abilities_by_executor = [await self.data_svc.locate('abilities', dict(executor=ex)) for ex in agent.executors]
+        capable_abilities = await agent.capabilities(list(itertools.chain.from_iterable(abilities_by_executor)))
+        return web.json_response([x.display for x in capable_abilities])
